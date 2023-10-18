5e9496239d74e5c0ce6144c83a78d680e5584f43
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2023-10-18 07:37:46 +0000

    feat: add constant for map actions and models texts

---
+++ resources/js/constants/notification.constant.ts
@@ -0,0 +1,11 @@
+import { NotificationAction, NotificationModel } from "../interfaces/notification.interface";
+
+export const NOTIFICATION_ACTIONS:Record<NotificationAction, string> = {
+    'commented': 'comentó',
+    'shared': 'compartió'
+};
+
+export const NOTIFICATION_MODELS:Record<NotificationModel, string> = {
+    'califications': 'Calificaciones',
+    'events': 'Eventos',
+};
