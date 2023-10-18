b095976ea6c7e014bc76dc281c107f7d70d46201
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2023-10-18 08:33:18 +0000

    feat: add filter for notifications

--- resources/js/Components/Amadeus/Notification/List.vue
+++ resources/js/Components/Amadeus/Notification/List.vue
@@ -4,22 +4,59 @@
         <div class="notification-container">
             <div class="notification-header">
                 <div class="notification-header-content">
-                    <div class="notification-header-title">Notifications</div>
+                    <div class="notification-header-title my-1">
+                        Notifications
+                    </div>
                     <div class="filter ml-3">
-                        <button class="filter-button text-xs">
-                            <div class="text-gray-400">Todo</div>
-                            <i class="fa-light fa-chevron-down text-gray-400" />
-                        </button>
+                        <Dropdown
+                            align="left"
+                            width="48"
+                            container-classes="border border-gray-300 rounded-md"
+                            content-classes="my-0"
+                        >
+                            <template #trigger>
+                                <button class="filter-button text-xs">
+                                    <div class="text-gray-400">
+                                        {{ modelFilter?.value || "Todos" }}
+                                    </div>
+                                    <i
+                                        class="fa-light fa-chevron-down text-gray-400"
+                                    />
+                                </button>
+                            </template>
+
+                            <template #content>
+                                <ul class="list-group">
+                                    <li
+                                        class="list-group-item text-sm"
+                                        v-for="model in uniqueModelNames"
+                                        :key="model.key"
+                                    >
+                                        <button
+                                            class="dropdown-item"
+                                            @click="selectModel(model)"
+                                        >
+                                            {{ model.value }}
+                                        </button>
+                                    </li>
+                                </ul>
+                            </template>
+                        </Dropdown>
                     </div>
                 </div>
             </div>
             <div class="notification-items">
-                <div v-if="notifications.length === 0" class="empty-state mx-auto py-3">
+                <div
+                    v-if="filteredNotifications.length === 0"
+                    class="empty-state mx-auto py-3"
+                >
                     <i class="fas fa-bell-slash text-xl"></i>
-                    <div class="text-gray-400">No hay notificaciones disponibles</div>
+                    <div class="text-gray-400">
+                        No hay notificaciones disponibles
+                    </div>
                 </div>
                 <NotificationListItem
-                    v-for="notification in notifications"
+                    v-for="notification in filteredNotifications"
                     :key="notification.id"
                     :user="notification.user"
                     :action="notification.action"
@@ -34,15 +71,46 @@
 </template>
 
 <script lang="ts" setup>
-import { PropType } from "vue";
+import { PropType, computed, ref, toRefs } from "vue";
 import NotificationListItem from "@/Components/Amadeus/Notification/ListItem.vue";
+import Dropdown from "@/Components/Amadeus/AmadeusDropdown.vue";
 import { INotification } from "@/interfaces/notification.interface";
+import { NOTIFICATION_MODELS } from "@/constants/notification.constant";
 
-defineProps({
+const props = defineProps({
     notifications: {
         type: Array as PropType<INotification[]>,
         required: true,
     },
+});
+const { notifications } = toRefs(props);
+const uniqueModelNames = computed(() => {
+    return [
+        {
+            key: "",
+            value: "Todo",
+        },
+    ].concat(
+        Object.entries(NOTIFICATION_MODELS).map(([key, value]) => ({
+            key,
+            value,
+        }))
+    );
+});
+
+const modelFilter = ref<{ key: string; value: string } | null>(null);
+const selectModel = (model: { key: string; value: string }) => {
+    modelFilter.value = model;
+};
+
+const filteredNotifications = computed(() => {
+    if (!modelFilter.value?.key) {
+        return notifications.value;
+    } else {
+        return notifications.value.filter(
+            (notification) => notification.model_name === modelFilter.value.key
+        );
+    }
 });
 </script>
 
@@ -148,9 +216,8 @@ defineProps({
 }
 
 .notification-list .empty-state {
-  text-align: center;
-  font-size: 16px;
-  color: #777;
+    text-align: center;
+    font-size: 16px;
+    color: #777;
 }
-
 </style>
