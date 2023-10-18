24a77d39999030b2dc0e48ae6d82aafed777d6f6
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2023-10-18 09:01:15 +0000

    refactor: extract notification list header to another component

--- resources/js/Components/Amadeus/Notification/List.vue
+++ resources/js/Components/Amadeus/Notification/List.vue
@@ -2,49 +2,9 @@
     <!-- click.stop prevent to close on click -->
     <div @click.stop="() => {}" class="notification-list">
         <div class="notification-container">
-            <div class="notification-header">
-                <div class="notification-header-content">
-                    <div class="notification-header-title my-1">
-                        Notifications
-                    </div>
-                    <div class="filter ml-3">
-                        <Dropdown
-                            align="left"
-                            width="48"
-                            container-classes="border border-gray-300 rounded-md"
-                            content-classes="my-0"
-                        >
-                            <template #trigger>
-                                <button class="filter-button text-xs">
-                                    <div class="text-gray-400">
-                                        {{ modelFilter?.value || "Todos" }}
-                                    </div>
-                                    <i
-                                        class="fa-light fa-chevron-down text-gray-400"
-                                    />
-                                </button>
-                            </template>
-
-                            <template #content>
-                                <ul class="list-group">
-                                    <li
-                                        class="list-group-item text-xs py-1"
-                                        v-for="model in uniqueModelNames"
-                                        :key="model.key"
-                                    >
-                                        <button
-                                            class="dropdown-item"
-                                            @click="selectModel(model)"
-                                        >
-                                            {{ model.value }}
-                                        </button>
-                                    </li>
-                                </ul>
-                            </template>
-                        </Dropdown>
-                    </div>
-                </div>
-            </div>
+            <NotificationListHeader
+                v-model="modelFilter"
+            ></NotificationListHeader>
             <div class="notification-items">
                 <NotificationListEmpty
                     v-if="filteredNotifications.length === 0"
@@ -67,36 +27,20 @@
 <script lang="ts" setup>
 import { PropType, computed, ref, toRefs } from "vue";
 import NotificationListItem from "@/Components/Amadeus/Notification/ListItem.vue";
-import Dropdown from "@/Components/Amadeus/AmadeusDropdown.vue";
+import NotificationListHeader from "@/Components/Amadeus/Notification/ListHeader.vue";
 import NotificationListEmpty from "@/Components/Amadeus/Notification/ListEmpty.vue";
 import { INotification } from "@/interfaces/notification.interface";
-import { NOTIFICATION_MODELS } from "@/constants/notification.constant";
+import { IBaseOption } from "@/interfaces/option.interface";
 
 const props = defineProps({
     notifications: {
-        type: Array as PropType<INotification[]>,
+        type: [Array, null] as PropType<INotification[]>,
         required: true,
     },
 });
 const { notifications } = toRefs(props);
-const uniqueModelNames = computed(() => {
-    return [
-        {
-            key: "",
-            value: "Todo",
-        },
-    ].concat(
-        Object.entries(NOTIFICATION_MODELS).map(([key, value]) => ({
-            key,
-            value,
-        }))
-    );
-});
 
-const modelFilter = ref<{ key: string; value: string } | null>(null);
-const selectModel = (model: { key: string; value: string }) => {
-    modelFilter.value = model;
-};
+const modelFilter = ref<IBaseOption | null>(null);
 
 const filteredNotifications = computed(() => {
     if (!modelFilter.value?.key) {
@@ -133,62 +77,6 @@ const filteredNotifications = computed(() => {
     flex: 0 0 auto;
 }
 
-.notification-list .notification-header {
-    gap: 94px;
-    padding: 12px 16px;
-    align-self: stretch;
-    width: 100%;
-    display: flex;
-    align-items: flex-start;
-    position: relative;
-    flex: 0 0 auto;
-}
-
-.notification-list .notification-header-content {
-    height: 21px;
-    position: relative;
-    flex: 1;
-    flex-grow: 1;
-}
-
-.notification-list .notification-header-title {
-    position: relative;
-    font-weight: 500;
-    color: #222038;
-    font-size: 14px;
-    letter-spacing: 0;
-    line-height: 20px;
-    white-space: nowrap;
-}
-
-.notification-list .filter {
-    display: inline-flex;
-    align-items: flex-start;
-    position: absolute;
-    top: 1px;
-    left: 94px;
-}
-
-.notification-list .filter-button {
-    display: inline-flex;
-    align-items: center;
-    gap: 4px;
-    position: relative;
-    flex: 0 0 auto;
-}
-
-.notification-list .all {
-    position: relative;
-    width: 44px;
-    margin-top: -1px;
-    font-weight: 500;
-    color: #9ea0aa;
-    font-size: 12px;
-    text-align: right;
-    letter-spacing: 0;
-    line-height: 20px;
-}
-
 .notification-list .notification-items {
     flex-direction: column;
     width: 100%;
@@ -196,17 +84,6 @@ const filteredNotifications = computed(() => {
     display: flex;
     align-items: flex-start;
     position: relative;
-    flex: 0 0 auto;
-}
-
-.notification-list .header-container {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    padding: 0px 0px 8px;
-    position: relative;
-    align-self: stretch;
-    width: 100%;
     flex: 0 0 auto;
 }
 </style>
---
+++ resources/js/Components/Amadeus/Notification/ListHeader.vue
@@ -0,0 +1,140 @@
+<template>
+    <div class="notification-header">
+        <div class="notification-header-content">
+            <div class="notification-header-title my-1">Notifications</div>
+            <div class="notification-header-filter ml-3">
+                <Dropdown
+                    align="left"
+                    width="48"
+                    container-classes="border border-gray-300 rounded-md"
+                    content-classes="my-0"
+                >
+                    <template #trigger>
+                        <button
+                            class="notification-header-filter-button text-xs"
+                        >
+                            <div class="text-gray-400">
+                                {{ modelSync?.value || "Todos" }}
+                            </div>
+                            <i class="fa-light fa-chevron-down text-gray-400" />
+                        </button>
+                    </template>
+
+                    <template #content>
+                        <ul class="list-group">
+                            <li
+                                class="list-group-item text-xs py-1"
+                                v-for="model in modelOptions"
+                                :key="model.key"
+                            >
+                                <button
+                                    class="dropdown-item"
+                                    @click="selectModel(model)"
+                                >
+                                    {{ model.value }}
+                                </button>
+                            </li>
+                        </ul>
+                    </template>
+                </Dropdown>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref, defineProps, computed, PropType } from "vue";
+import Dropdown from "@/Components/Amadeus/AmadeusDropdown.vue";
+import { NOTIFICATION_MODELS } from "@/constants/notification.constant";
+import { IBaseOption } from "@/interfaces/option.interface";
+
+const props = defineProps({
+    modelValue: {
+        type: Object as PropType<IBaseOption>,
+        required: true,
+    },
+});
+const emit = defineEmits(["update:modelValue"]);
+const modelOptions = computed(() => {
+    return [
+        {
+            key: "",
+            value: "Todo",
+        },
+    ].concat(
+        Object.entries(NOTIFICATION_MODELS).map(([key, value]) => ({
+            key,
+            value,
+        }))
+    );
+});
+const modelSync = computed({
+    get() {
+        return props.modelValue;
+    },
+    set(value: IBaseOption) {
+        emit("update:modelValue", value);
+    },
+});
+
+const selectModel = (model: IBaseOption) => {
+    modelSync.value = model;
+};
+</script>
+
+<style scoped>
+.notification-list .notification-header {
+    gap: 94px;
+    padding: 12px 16px;
+    align-self: stretch;
+    width: 100%;
+    display: flex;
+    align-items: flex-start;
+    position: relative;
+    flex: 0 0 auto;
+}
+
+.notification-list .notification-header-content {
+    height: 21px;
+    position: relative;
+    flex: 1;
+    flex-grow: 1;
+}
+
+.notification-list .notification-header-title {
+    position: relative;
+    font-weight: 500;
+    color: #222038;
+    font-size: 14px;
+    letter-spacing: 0;
+    line-height: 20px;
+    white-space: nowrap;
+}
+
+.notification-list .notification-header-filter {
+    display: inline-flex;
+    align-items: flex-start;
+    position: absolute;
+    top: 1px;
+    left: 94px;
+}
+
+.notification-list .notification-header-filter-button {
+    display: inline-flex;
+    align-items: center;
+    gap: 4px;
+    position: relative;
+    flex: 0 0 auto;
+}
+
+.notification-list .header-container {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 0px 0px 8px;
+    position: relative;
+    align-self: stretch;
+    width: 100%;
+    flex: 0 0 auto;
+}
+</style>
